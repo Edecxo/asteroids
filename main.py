cf9d9beb1bf8cfd48654a010d7e465ff89531348
Edecxo <hunterlogan.eden@gmail.com>
2025-01-10 14:58:36 -0600

    added tick rate to limit the fps

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import pygame
 from constants import *
 
 def main():
+    #initialize the game and necessary objects/variables
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 #delta time
+    fps = 60
 
+    #game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(fps) / 1000
 
 if __name__ == "__main__":
     main()
