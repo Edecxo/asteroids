82ec261134d6c3e3fa3cbd8a4e2a442a61e8de2e
Edecxo <hunterlogan.eden@gmail.com>
2025-01-11 00:53:31 -0600

    added player model

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     #initialize the game and necessary objects/variables
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
     dt = 0 #delta time
-    fps = 60
 
     #game loop
     while True:
@@ -15,8 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(fps) / 1000
+        dt = clock.tick(FPS) / 1000
 
 if __name__ == "__main__":
     main()
